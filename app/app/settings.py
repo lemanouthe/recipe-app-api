39b038deaf168e98b12c7c19bee67cda76432715
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2022-07-22 12:13:34 +0000

    Configured docker compose and checks to wait for db

--- app/app/settings.py
+++ app/app/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,18 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": str(os.environ.get("DB_HOST")),
+        "NAME": str(os.environ.get("DB_NAME")),
+        "USER": str(os.environ.get("DB_USER")),
+        "PASSWORD": str(os.environ.get("DB_PASS")),
+        # "PORT": str(os.environ.get("DB_PORT")),
+    },
 }
 
 
