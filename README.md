26794f0bcd9335a7e190beca4b55bc8687390a30
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2022-08-05 17:41:24 +0000

    advanced with test

--- README.md
+++ README.md
@@ -10,5 +10,12 @@ docker-compose run --rm app sh -c "flake8"
 # crée le projet django
 docker-compose run --rm app sh -c "django-admin startproject app ."
 
+# lance les test de django
+docker-compose run --rm app sh -c "python manage.py test"
+
 # démarrer les services docker
 docker-compose up
+
+## User information
+test@example.com
+test
---
+++ app/recipe/views.py
@@ -0,0 +1,93 @@
+"""
+Views for the recipe APIs.
+"""
+from rest_framework import (
+    viewsets,
+    mixins,
+    status,
+)
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import (
+    Recipe,
+    Tag,
+    Ingredient,
+)
+from recipe.serializers import (
+    RecipeSerializer,
+    RecipeDetailSerializer,
+    TagSerializer,
+    IngredientSerializer,
+    RecipeImageSerializer
+)
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """View for manage recipe APIs."""
+
+    serializer_class = RecipeDetailSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-id")
+
+    def get_serializer_class(self):
+        """Return the serializer class for request."""
+        if self.action == "list":
+            return RecipeSerializer
+        elif self.action == "upload_image":
+            return RecipeImageSerializer
+
+        return self.serializer_class
+
+    def perform_create(self, serializer):
+        """Create a new Recipe."""
+        serializer.save(user=self.request.user)
+
+    @action(methods=['POST'], detail=True, url_path="upload_image")
+    def upload_image(self, request, pk=None):
+        """Upload an image to recipe."""
+        recipe = self.get_object()
+        serializer = self.get_serializer(recipe, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class BaseRecipeAttrViewset(
+    mixins.DestroyModelMixin,
+    mixins.UpdateModelMixin,
+    mixins.ListModelMixin,
+    viewsets.GenericViewSet,
+):
+    """Base viewsets for recipe attributes."""
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by("-name")
+
+
+class TagViewSet(BaseRecipeAttrViewset):
+    """View for manage tags APIs."""
+
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+
+class IngredientViewSet(BaseRecipeAttrViewset):
+    """View for manage ingredients APIs."""
+
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
