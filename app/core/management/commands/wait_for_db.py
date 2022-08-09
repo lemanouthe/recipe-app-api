f69a50ba845e10bf61f6b39f973ba74f78c1853b
lemanouthe <47214761+lemanouthe@users.noreply.github.com>
2022-08-09 15:51:56 +0000

    update commande wait_for_bd

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,7 +21,11 @@ class Command(BaseCommand):
                 self.check(databases=["default"])
                 db_up = True
             except (Psycopg2Error, OperationalError):
-                self.stdout.write("Database unavailable, waiting 1 second...")
+                self.stdout.write(
+                    self.style.WARNING(
+                        "Database unavailable, waiting 1 second..."
+                    ),
+                )
                 time.sleep(1)
 
         self.stdout.write(self.style.SUCCESS("Database is available!"))
